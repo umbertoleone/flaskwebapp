6c743637bed5aff823f84823eb6155605ddce2ed
Umberto Leone <leon.fukasawa@gmail.com>
2021-02-06 10:04:47 -0800

    added classes, messages and some bootstrap

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+# from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'akutenshi'
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
     # Import views and auth
     from .views import views
     from .auth import auth
@@ -10,5 +20,13 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
+    create_database(app)
+
+
     return app
 
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database')
