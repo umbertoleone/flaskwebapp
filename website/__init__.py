ace8c9db4b6acfc34a1e07a4867685719105f917
Umberto Leone <leon.fukasawa@gmail.com>
2021-02-06 11:15:56 -0800

    added database, sign up and login auths

--- website/__init__.py
+++ website/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 # from flask_login import LoginManager
 
+# create an object db
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
@@ -10,23 +11,27 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    #configure the database
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    # initialize database
     db.init_app(app)
 
     # Import views and auth
     from .views import views
     from .auth import auth
+
     # register blueprints
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    # checks if we have created our database
     from .models import User, Note
     create_database(app)
-
 
     return app
 
 def create_database(app):
     if not path.exists('website/' + DB_NAME):
+        
         db.create_all(app=app)
-        print('Created Database')
+        print('Created Database!')
--- website/models.py
+++ website/models.py
@@ -1,11 +1,14 @@
 from . import db
-from flask_login import UserMixin
+# user mixin module
+from flask_login import UserMixin 
 from sqlalchemy.sql import func
 
+#schemas one user has many notes
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
+    # one user many notes
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
@@ -14,5 +17,5 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    #allows our user to find all his notes
+    #allows our users to find all his notes
     notes = db.relationship('Note')
